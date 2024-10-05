ddb310591fc1342aa5ff006b087b0b4ce1de0a1c
YehyunSoftchoice <glad7cu@gmail.com>
2024-10-05 00:52:13 -0400

    Home page with auth

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,115 +1,96 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+// import Link from "next/link";
 import Image from "next/image";
-import localFont from "next/font/local";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { api } from "~/utils/api";
+import TrashcamLeaderboard from "~/components/trashcamLeaderboard";
+
+const users: [string, number][] = [
+  ['user1', 10],
+  ['user2', 15],
+  ['user3', 20],
+  ['user4', 20],
+  ['user5', 20]
+];
+
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <>
+      <Head>
+        <title>Trashcam</title>
+        <meta name="description" content="Trashcam website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <div className="snap-y snap-mandatory">
+          <div className="bg-F5FAFA snap-start">
+            <div className="flex flex-col items-center gap-2 pb-4" data-aos="fade-up">
+              <p className="text-1E635F mt-6 text-5xl font-extrabold">Welcome to Trashcam!</p>
+              <p className="text-507371 text-2xl mb-4">
+                ...
+              </p>
+            </div>
+
+
+            <div className="flex flex-col items-center gap-2 text-black">
+              <AuthShowcase />
+            </div>
+
+          </div>
+
+          <div className="bg-DAF2F1 h-381 flex w-full flex-col items-center gap-2">
+            <div style={{ transform: 'scale(1)' }}>
+              <Image
+                src="/landingpage.png"
+                alt="Landing Page"
+                width={780}
+                height={742}
+              />
+            </div>
+          </div>
+          <div className="bg-F5FAFA snap-center">
+            <h1 className='text-1E635F text-center pt-[2rem] mb-[1rem] text-3xl font-bold' >Leaderboard</h1>
+            <TrashcamLeaderboard />
+          </div>
+          <div className="bg-F5FAFA snap-start">
+            <div className="textBlock text-1E635F bg-F5FAFA flex pt-8 pb-8 w-[60vw] flex-col mr-[auto] ml-[auto] [&>*]:mb-[0.5rem]">
+              <h2 data-aos="fade-right" className="text-3xl font-bold"> What is Trashcam?</h2>
+              <div data-aos="fade-down-right">
+                <p>...</p>
+              </div>
+              <div data-aos="fade-up-right">
+                <p>...</p>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+    </>
+  );
+}
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+  const callbackUrl = '/play';
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 pb-4">
+      <p className="text-center text-2xl text-black">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+      </p>
+      <button
+        className="rounded-full bg-1E635F text-white px-10 py-3 font-semibold no-underline transition hover:bg-white hover:text-1E635F hover:bg-opacity-20"
+        onClick={sessionData ? () => signOut() : () => signIn('credentials', { callbackUrl })}
+
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+
     </div>
+
   );
 }
