d868bcf92dab83c34443e38f7c734ca1f98870ae
YehyunSoftchoice <glad7cu@gmail.com>
2024-10-05 01:20:07 -0400

    LOGO

--- app/src/pages/pages/_app.tsx
+++ app/src/pages/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import Nav from '../components/nav';
+import Nav from '../../components/nav';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
