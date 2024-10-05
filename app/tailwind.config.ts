033de316b89b566ed8ff36926f026b7e7487e205
YehyunSoftchoice <glad7cu@gmail.com>
2024-10-04 23:09:34 -0400

    Auth setup

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,19 +1,36 @@
-import type { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        '1E635F': '#1E635F',
+        'F5FAFA': '#F5FAFA',
+        'DAF2F1' : '#DAF2F1'
+        },
+      keyframes: {
+        slideleft: {
+          '0%': { transform: 'translateX(-100px)' },
+          '10%': { transform: 'translateX(-90px)' },
+          '20%': { transform: 'translateX(-80px)' },
+          '30%': { transform: 'translateX(-70px)' },
+          '40%': { transform: 'translateX(-60px)' },
+          '50%': { transform: 'translateX(-50px)' },
+          '60%': { transform: 'translateX(-60px)' },
+          '100%': { transform: 'translateX(0px)' },
+        },
+      },
+      animation: {
+        'slideleft': 'slideleft',
+      },
+      
     },
+    
   },
   plugins: [],
-};
-export default config;
+} satisfies Config;
